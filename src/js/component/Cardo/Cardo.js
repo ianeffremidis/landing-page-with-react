bff3ee05f495c071584a4111a30c308a526f2b4c
ianeffremidis <ianeffremidis@gmail.com>
2022-12-07 10:00:05 +0000

    2nd commit

--- src/js/component/Cardo/Cardo.js
+++ src/js/component/Cardo/Cardo.js
@@ -4,16 +4,15 @@ import styles from './Cardo.module.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-const Cardo = () => (
+const Cardo = (props) => (
   <div className={styles.Cardo}>
 
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="http://via.placeholder.com/640x360" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{props.title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {props.text}
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -21,10 +21,15 @@ const Home = () => {
 					<div className="col"><Jumbotron /></div>
 				</div>
 				<div className="row p-2 d-flex text-center justify-content-lg-between justify-content-center ">
-				<Cardo/>
-				<Cardo/>
-				<Cardo/>
-				<Cardo/>
+				<Cardo title="George" text="Lorem blabla yusuf boaasgagsasia asdjas"/>
+				<Cardo title="Takis" text="Lorem basd yusuf boasia asdjasaeraeraera aer"/>
+				<Cardo title="Bill" text="Lorem blabla yusuf boasia asdasdadasdasdjas"/>
+				<Cardo title="Kostas" text="Lorem blaasdadyusuf boasiasdasdasdaasda asdjas"/>
+				<Cardo title="Caca" text="Lorem blabla yusugasgasgf boasia asdjas"/>
+				<Cardo title="Pupou" text="Loremasdasdbla yusuf boasia asdjas"/>
+				<Cardo title="vasvas" text="Loreasddbla yuasdadasdasuf boasia asdjas"/>
+				<Cardo title="PKers" text="Loreasddbla yusuf boasia asdjasdasdasdasdaas"/>
+				
 				</div>
 				
 			</div>
